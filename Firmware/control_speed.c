b127de44f3209163bfceb9512fc4cb874387d9d7
LosVocelos <janocelik@gmail.com>
2025-07-31 17:27:29 +0200

    Removed pico-sdk submodule

--- Firmware/control_speed.c
+++ Firmware/control_speed.c
@@ -5,18 +5,29 @@
 #include <stdint.h>
 #include <stdio.h>
 
-PID_t motor_pid = {
-    .kp = 2.0,
-    .ki = 1.0,
-    .kd = 0,
-};
+PID_t motors_pid[MOTOR_COUNT] = { [0 ... MOTOR_COUNT-1] = {
+    .kp = 5,
+    .ki = 0.0,
+    .kd = 0.0,
+}};
 
-int compute_encoder_speed(uint8_t encoder){
+float compute_encoder_rpm(uint8_t encoder, float delta_ms){
     static int last_count[ENCODER_COUNT] = {0};
     int count_now = encoder_get_count(encoder);
     int delta_count = count_now - last_count[encoder];
     last_count[encoder] = count_now;
-    return delta_count;
+    int measured_speed = (float) delta_count / delta_ms * 1.0e3;
+    return (float) measured_speed / 617.3544 * 60.0;
+}
+
+float filter_speed_rpm(uint8_t encoder, float v) {
+    static float v_prev[ENCODER_COUNT] = {0};
+    static float v_filt[ENCODER_COUNT] = {0};
+
+    v_filt[encoder] = 0.854 * v_filt[encoder] + 0.0728 * v + 0.0728 * v_prev[encoder];
+    v_prev[encoder] = v;
+
+    return  v_filt[encoder];
 }
 
 int clamp_int(int val, int min, int max) {
@@ -26,19 +37,23 @@ int clamp_int(int val, int min, int max) {
 }
 
 void control_motor_speed(int target_speed, uint8_t side, float delta_ms){
-    int measured_speed = compute_encoder_speed(side);
-    printf("Encoder speed: %i delta: %f\n", measured_speed, delta_ms);
-    motor_pid.measured_value = measured_speed;
-    motor_pid.dt = delta_ms;
-    motor_pid.setpoint = target_speed;
-    pid_compute(&motor_pid);
-    int pwm_value = motor_pid.output * 1000;
+    PID_t* motor_pid = &motors_pid[side];
+    float measured_rpm = compute_encoder_rpm(side, delta_ms);
+    float filtered_rpm = filter_speed_rpm(side, measured_rpm);
+    // printf("Encoder speed: %i delta: %f\n", measured_speed, delta_ms);
+    printf("%f,%i,", filtered_rpm, target_speed);
+    motor_pid->measured_value = filtered_rpm;
+    motor_pid->dt = delta_ms;
+    motor_pid->setpoint = target_speed;
+    pid_compute(motor_pid);
+    int pwm_value = motor_pid->output * 100;
     pwm_value = clamp_int(pwm_value, 0, 0xFFFF);
-    motor_set_pwm(side, DIRECTION_FORWARD, 0.5*0xFFFF);
+    motor_set_pwm(side, DIRECTION_FORWARD, pwm_value);
 }
 
-void control_speed(uint16_t target_speed[2], float delta_ms) {
+void control_speed(uint16_t target_speed[MOTOR_COUNT], float delta_ms) {
     for (int motor; motor < MOTOR_COUNT; motor++) {
         control_motor_speed(target_speed[motor], motor, delta_ms);
     }
+    printf("\n");
 }
--- Firmware/main.c
+++ Firmware/main.c
@@ -11,13 +11,14 @@ enum command {
     COMMAND_COAST
 };
 
-uint16_t control[2] = {30, 30};
+uint16_t control[2] = {0, 0};
 
 int main() {
     motor_init();
     stdio_usb_init();
     encoder_init();
     absolute_time_t last_time = get_absolute_time();
+    int wrum_time = last_time;
     while (true) {
         absolute_time_t current_time = get_absolute_time();
         int64_t delta_us = absolute_time_diff_us(last_time, current_time);
@@ -27,6 +28,17 @@ int main() {
             control_speed(control, delta_us / 1000.0);
             break;
         }
-        sleep_ms(50);
+        sleep_ms(5);
+
+        if (current_time - wrum_time > 5e6) {
+            wrum_time = current_time;
+            if (control[0] == 0) {
+                control[0] = 60;
+                control[1] = 60;
+            } else {
+                control[0] = 0;
+                control[1] = 0;
+            }
+        }
     }
 }
